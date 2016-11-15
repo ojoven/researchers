578c6eb00de621444aed2f4f4de35981f2a0e06c
ojoven <mikeltorresmail@gmail.com>
2016-11-15 18:31:12 +0100

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+// VARS
+var $width, $height, $orientation;
+
+// LOGIC
+$(document).ready(function() {
+
+    // Fast Click
+    FastClick.attach(document.body);
+
+});
+
+$(window).resize(function() {
+
+
+});
+
+// FUNCTIONS
